a36453c63c90752d0d65794f844c2fa40ea5de3b
Ikazu <96537985+ikazuGt@users.noreply.github.com>
2025-12-31 16:34:19 +0700

    Update win_install.sh

--- win_install.sh
+++ win_install.sh
@@ -40,7 +40,7 @@ echo "  8) Custom Link"
 read -p "Select [1]: " PILIHOS
 
 case "$PILIHOS" in
-  1|"") PILIHOS="https://download1590.mediafire.com/12h6jiznxx3gYnHzYDA1yDBRKy9BUJuObBqAYd5PYL73SI6VvkfRrqKmQW8Hs5qwS5XOpRh-S5sx16cXDJXQWWsPABZUW3oC01ev-dGFr1r-8aFYxAbnT_tFVZVA-EVAf9gJ2J8eQZSVgTtbWVSnNK0FSAGSCokhdshWzIejBlNWEwo/5bnp3aoc7pi7jl9/windows2019DO.gz";;
+  1|"") PILIHOS="https://download1590.mediafire.com/lfn6u0ivb9lgASqVpZTL3IKDFXrCj8M9l6vRUcpp2c5vXVOZqr5r75j-OsspcxII5wh3svP2pqBbte3bteApHchf08R0S6k4L73rXY_zO3cZGeu3JeGSNkHfYg81K4FST92T-xfmOBeprhDFeSDLHioZAhig2eKZmNg658J_GTHk1BM/5bnp3aoc7pi7jl9/windows2019DO.gz";;
   2) PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows2019DO.gz";;
   3) PILIHOS="https://master.dl.sourceforge.net/project/manyod/wedus10lite.gz?viasf=1";;
   4) PILIHOS="https://download1582.mediafire.com/lemxvneeredgyBT5P6YtAU5Dq-mikaH29djd8VnlyMcV1iM_vHJzYCiTc8V3PQkUslqgQSG0ftRJ0X2w3t1D7T4a-616-phGqQ2xKCn8894r0fdV9jKMhVYKH8N1dXMvtsZdK6e4t9F4Hg66wCzpXvuD_jcRu9_-i65_Kbr-HeW8Bw/gcxlheshfpbyigg/wedus10lite.gz";;
