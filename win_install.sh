3daf22f044274ea1e4c3702db23e176d66bba2a0
Ikazu <96537985+ikazuGt@users.noreply.github.com>
2026-01-18 18:07:56 +0700

    Update win_install.sh

--- win_install.sh
+++ win_install.sh
@@ -42,7 +42,7 @@ echo "  10) Custom Link"
 read -p "Select [1]: " PILIHOS
 
 case "$PILIHOS" in
-  1|"") PILIHOS="https://download1590.mediafire.com/vnyrfz85eqbgeGui3YXjKEbsZZLh4pjRKagJ5P4Kzn9nSKwFrVdbEy54GIbtuqvCjZJ_oW73UsD8LcLujp7KYxPj8I1h2vTA3GHAh1Y1Xtlgzyofr5QTa2wWXVRyUZWYF6wv31rEghE-PqH_cDAkEL0aPI2Q-J-29lcevQtmPi-3KAlT/5bnp3aoc7pi7jl9/windows2019DO.gz";;
+  1|"") PILIHOS="https://download1590.mediafire.com/oojr05urudpgn54zCZdjExCAHIv_c_LGw9gRRcoYazoTC-q8oybMiUQN116vUEmyxRMSzlR0UJFRTY_3w4aTV6Q2rku1sa-FBLqh5XOdDOfo53trfqhc-Zb8pqVDrRn1O1jH_YRA7pSm5Sv4-EeoiBcqi7LuGHhw59dHdjPpnYcPXO6x/5bnp3aoc7pi7jl9/windows2019DO.gz";;
   2) PILIHOS="https://pixeldrain.com/api/file/Cx29Sb9H";;
   3) PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows2016.gz/download";;
   4) PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows2012.gz/download";;
